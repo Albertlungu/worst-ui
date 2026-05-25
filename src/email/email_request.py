dcfa7648047019dbe45bf000f102953713354b14
MathisFun321 <mathisguitarboy@gmail.com>
2026-05-25 12:32:29 -0400

    feat: make some of the email sending backend, still have to connect it to the html

---
+++ src/email/email_request.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.message import EmailMessage
+
+def setup_message(content: str, subject: str, sender: str, recipient: str) -> EmailMessage:
+    msg = EmailMessage()
+    msg.set_content(content)
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = recipient
+
+    return msg
+
+def send_message(message: EmailMessage):
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login("albert.lungu.2010@gmail.com", "rndbdtpsvujjkocc")
+            server.send_message(message)
+        print("Check your inbox")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def main():
+    message = setup_message(
+        content = "this is a test",
+        subject = "this is a test",
+        sender = "albert.lungu.2010@gmail.com",
+        recipient = "albert.lungu.2010@gmail.com"
+        )
+    send_message(message)
+
+if __name__ == "__main__":
+    main()
